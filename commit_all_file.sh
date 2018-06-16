bc76d1de89d00ec6fc0058c10eae68a9e5ae77ff
Tal Tal <vothanhlam1793@gmail.com>
2018-06-16 23:45:18 +0700

    Merge brand1

--- commit_all_file.sh
+++ commit_all_file.sh
@@ -1,8 +1,8 @@
 #https://stackoverflow.com/questions/2388090/how-to-delete-and-replace-last-line-in-the-terminal-using-bash
 echo $1 - $2
-CMD=$1
+CMD=$2
 ROOT=origin
-BRAND=brand1
+BRAND=$1
 git add .
 git commit -m "$CMD"
 echo "10 seconds to push ...."
---
+++ pullMulti.sh
@@ -0,0 +1,1 @@
+git pull git@github.com:vothanhlam1793/multi.git
